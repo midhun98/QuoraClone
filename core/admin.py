1adaaadff0a4a5d6632f3ba5822de1b7e9d763f8
Midhun <midhunskani@gmail.com>
2023-10-31 20:39:10 +0530

    Regiter the models

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,36 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from core import models
+from .forms import CustomUserChangeForm, CustomUserCreationForm
+from .models import CustomUser
 
 # Register your models here.
+
+
+class CustomUserAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    filter_horizontal = ('groups', 'user_permissions')
+
+    model = CustomUser
+    list_display = ("phone_number", "email", "is_staff", "is_active",)
+    list_filter = ("is_staff", "is_active", "groups",)
+    fieldsets = (
+        (None, {"fields": ("phone_number", "email", "password")}),
+        ("Personal Info", {"fields": ("first_name", "last_name", "profile_picture", "date")}),
+        ("Permissions", {"fields": ("is_staff", "is_active", "groups", "user_permissions")}),
+    )
+    add_fieldsets = (
+        (None, {
+            "classes": ("wide",),
+            "fields": (
+                "phone_number", "password1", "password2", "is_staff",
+                "is_active", "groups", "user_permissions"
+            )}
+         ),
+    )
+    search_fields = ("phone_number",)
+    ordering = ("phone_number",)
+
+
+admin.site.register(models.CustomUser, CustomUserAdmin)
--- quora/models.py
+++ quora/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from core.models import (
+    CustomUser
+)
 
 # Create your models here.
+
+
+class Question(models.Model):
+    title = models.CharField(max_length=255)
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+class Answer(models.Model):
+    content = models.TextField()
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+class Like(models.Model):
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    answer = models.ForeignKey(Answer, on_delete=models.CASCADE, null=True, blank=True)
+    date_liked = models.DateTimeField(auto_now_add=True)
